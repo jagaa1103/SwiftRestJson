dfdbcb4641a49ae04bdce744bdf9db9bac79794a
Jagaa <jagaa1103@gmail.com>
2016-01-22 17:52:41 +0100

    init Project and add cocoa pods

---
+++ SwiftRestJson/SwiftRestJson/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  SwiftRestJson
+//
+//  Created by Enkhjargal Gansukh on 1/22/16.
+//  Copyright © 2016 Enkhjargal Gansukh. All rights reserved.
+//
+
+import UIKit
+import SwiftyJSON
+import Alamofire
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func fromServer(){
+        Alamofire.request(.GET, "http://jsonplaceholder.typicode.com")
+            .responseJSON { response in
+                print(response.request)  // original URL request
+                print(response.response) // URL response
+                print(response.data)     // server data
+                print(response.result)   // result of response serialization
+                
+                if let JSON = response.result.value {
+                    print("JSON: \(JSON)")
+                }
+        }
+    }
+}
+
